a434871d086b4e31614a5f6c5c9934e01f91bf5c
Ruan <ruangustavo50711@gmail.com>
2020-11-11 16:30:46 -0300

    Algoritmo de soma

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Primeiro_Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro numero: ");
+            string num1_texto = Console.ReadLine();
+            int num1 = int.Parse(num1_texto);
+
+            Console.WriteLine("Digite o segundo numero");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int soma = num1 + num2;
+            Console.Write("O resultado: ");
+            Console.WriteLine(soma);
+        }   
+    }
+}
